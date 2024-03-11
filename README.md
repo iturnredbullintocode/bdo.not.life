a0c5c481b73694388f49b9902d649ef467bf2b11
Anna <anna.zuban.dev@gmail.com>
2024-03-10 20:05:54 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+Sometimes I worry that the applications I code are too portable but
+I decided to make this public for now. May private later.
+
+There do not seem to be any websites for BD Mobile that are focused on easily viewable
+written content, most are filled with video tutorials, which are not intuitive to consume.
+I aim to fill this gap and use this website to test one of my handwritten private PHP
+frameworks on a medium audience of users.
+
+
+Current Objectives
+==================
+- porting my genshin app into here and modifying as bdm
+- bdm discord
+- getting repo into a perfect and clean state
+
+My apologies for using this readme to brainstorm, but I'd like the users to see my thought process today.
+
+
+General Notes for Readers
+=========================
+🍑 I do play BDM, but I've only played for 2 weeks. I'm only 36k cp.
+I main a spiritwalker. I used to play BDO back when it first launched, and run a BDO
+database website.
+
+Structure
+=========
+- "application" folder is the only one that gets uploaded in its entirety to a PHP host server
+- "resources" is for items that aid with website creation but aren't meant to be served online,
+such my original photoshop layered images for the UI
+- everything outside of these two folders is meant to be kept to a minimum to prevent messiness
---
+++ application/index.php
@@ -0,0 +1,47 @@
+bdm stuff incoming here lol<br>
+test test test
+
+sources of cp:
+black sun --------------------------> edana coins ----> black rock shrine
+20min desert farming per day ----> edana coins ----> black rock shrine
+daily 24/7 afk farming
+4 world bosses per week
+40min everfrost farming per day (counting tea from pearl shop and wanted missions
+2 hrs chaos rift per week
+eventsss/giftsss/daily-logins
+merchantry once a day --> black pearls
+alyaelli bosses once a week
+hadum tower dispatch once a day
+patrigio delivery gives gifts once per week
+genoveve once per week ish
+black shrine once per week (5 tickets)
+pirate island onces per week
+glory once per week (7 tickets)
+collecting nodes with workers --> manufacturing delivery and patrigio delivery
+collecting nodes by hand to lvl up --> patrigio delivery item acquisition unlocked
+ramo 5 boxes per week
+ranked 3 wins per week
+battle of the sun 3 wins per week
+daily patrigio buyout
+2x daily bounties
+2x daily black spirit quests
+crafting cron stones, stamina, outfits, in camp
+morco (i dont have him unlocked yet tho)
+elten defense (just unlocked barely)
+murrowakabyrybth (not unlocked yet)
+buying stuff from other players off marketplace
+monthly desert reset mirages and temples
+weekly co-op rush
+weekly ice fishing 20 smelt
+atumach, vengeful souls, node war, siege, all not unlocked lol
+ig real money spending in shop is also a source i should write down for the sake of completionism
+daily stamina recovery and stamina blessing in camp
+manufacturing ---> manu delivery
+manufacturing ---> caphras stones and totems
+
+fairy (album, brooch, resonance)
+collection
+pet album
+elite trials
+general open world knowledge
+main (and side) questline
